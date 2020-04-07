52e2e6ff15330d04e63f7ad8a36f2079bc262c6a
angela <angela@matic.network>
2020-04-08 01:37:03 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# mytoken
+# MyToken Tutorial
+
+Built for, Matic MasterClass April 8, 2020. 
+
+Built upon, https://www.trufflesuite.com/boxes/tutorialtoken
--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,12 +3,5 @@ pragma solidity 0.5.16;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract TutorialToken is ERC20 {
-  string public name = "MyToken";
-  string public symbol = "MT";
-  uint8 public decimals = 2;
-  uint public INITIAL_SUPPLY = 12000;
-
-  constructor() public {
-    _mint(msg.sender, INITIAL_SUPPLY);
-  }
+  // add your token code here
 }
