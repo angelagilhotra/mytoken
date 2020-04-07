1382e90f9b5c472c243c191ae367546631f2d0f7
angela <angela@matic.network>
2020-04-08 02:47:09 +0530

    update metamask provider

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,22 @@
 App = {
   web3Provider: null,
   contracts: {},
+  web3js: null,
 
   init: function() {
     return App.initWeb3();
   },
 
-  initWeb3: function() {
-    // Initialize web3 and set the provider to the testRPC.
-    if (typeof web3 !== 'undefined') {
-      App.web3Provider = web3.currentProvider;
-      web3 = new Web3(web3.currentProvider);
-    } else {
-      // set the provider you want from Web3.providers
-      App.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545');
-      web3 = new Web3(App.web3Provider);
+  initWeb3: async function() {
+    if (typeof window.ethereum !== 'undefined') {
+      App.web3Provider = window['ethereum']
     }
-
+    App.web3js = new Web3 (App.web3Provider)
     return App.initContract();
   },
 
   initContract: function() {
-    $.getJSON('TutorialToken.json', function(data) {
+    $.getJSON('MyToken.json', function(data) {
       // Get the necessary contract artifact file and instantiate it with truffle-contract.
       var TutorialTokenArtifact = data;
       App.contracts.TutorialToken = TruffleContract(TutorialTokenArtifact);
@@ -50,7 +45,7 @@ App = {
 
     var tutorialTokenInstance;
 
-    web3.eth.getAccounts(function(error, accounts) {
+    App.web3js.eth.getAccounts(function(error, accounts) {
       if (error) {
         console.log(error);
       }
@@ -75,7 +70,7 @@ App = {
 
     var tutorialTokenInstance;
 
-    web3.eth.getAccounts(function(error, accounts) {
+    App.web3js.eth.getAccounts(function(error, accounts) {
       if (error) {
         console.log(error);
       }
